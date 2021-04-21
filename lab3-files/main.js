3d30469d2574d49e8d6567ffdabc09c9601efa57
r-jo <rdrsjck+gh@gmail.com>
2021-04-21 08:29:09 -0700

    Added fetch from url function. Profile picture not displaying

--- lab3-files/main.js
+++ lab3-files/main.js
@@ -1,12 +1,45 @@
 const url =
   "http://twitterfeedserverrails-env.eba-xmqy8ybh.us-east-1.elasticbeanstalk.com/feed/random?q=weather";
 
+// fetch template
 fetch(url)
   .then((res) => res.json())
-  .then((data) => {
     // do something with data
-  })
   .catch((err) => {
     // error catching
     console.log(err);
   });
+
+// button to fetch data
+document.getElementById("button1").addEventListener("click", loadJSON);
+
+function loadJSON() {
+  fetch(url)
+    // returns json response
+    // somehow already knows how to pull 10 tweets
+    .then(function (response) {
+      return response.json();
+    })
+    // returns data of the json
+    .then(function (data) {
+      let html = "";
+      data.statuses.forEach(function (tweet) {
+        html += `
+            <div class="tweet">
+          <img
+            src=${tweet.user.profile_image_url_https}"
+            alt="Profile Picture"
+            class="profile-picture"
+          />
+          <div>
+            <p><strong>${tweet.user.screen_name} </strong><span>${tweet.created_at}</span></p>
+            <caption>
+            ${tweet.text}
+            </caption>
+          </div>
+        </div>
+          `;
+      });
+      document.getElementById("tweets").innerHTML = html;
+    });
+}
