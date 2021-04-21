7a20d1f8596656a5178d57897975e81b83102186
r-jo <rdrsjck+gh@gmail.com>
2021-04-21 07:36:50 -0700

    Lab 3 initial commit

---
+++ lab3-files/main.js
@@ -0,0 +1,12 @@
+const url =
+  "http://twitterfeedserverrails-env.eba-xmqy8ybh.us-east-1.elasticbeanstalk.com/feed/random?q=weather";
+
+fetch(url)
+  .then((res) => res.json())
+  .then((data) => {
+    // do something with data
+  })
+  .catch((err) => {
+    // error catching
+    console.log(err);
+  });
