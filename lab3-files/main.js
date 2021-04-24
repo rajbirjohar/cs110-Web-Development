b505ab08394f43237a4451d69f2bfd65a4cc6baf
Matthew Lee <mattlee525@gmail.com>
2021-04-24 15:08:02 -0700

    Implemented button to toggle fetch

--- lab3-files/main.js
+++ lab3-files/main.js
@@ -3,6 +3,10 @@
 // Search bar
 // Remove duplicates
 
+// global vars
+let togAutoF = true;
+
+
 const url =
   "http://twitterfeedserverrails-env.eba-xmqy8ybh.us-east-1.elasticbeanstalk.com/feed/random?q=weather";
 
@@ -18,36 +22,50 @@ fetch(url)
 // button to fetch data
 document.getElementById("button1").addEventListener("click", loadJSON);
 // button to pause fetching
+document.getElementById("button2").addEventListener("click", toggleFetch);
 
+
+// Start loading JSON data
 function loadJSON() {
-//   const interval = setInterval(function () {
-    fetch(url)
-      // returns json response
-      // somehow already knows how to pull 10 tweets
-      .then(function (response) {
-        return response.json();
-      })
-      // returns data of the json
-      .then(function (data) {
-        let html = "";
-        data.statuses.forEach(function (tweet) {
-          html += `
+  
+    const interval = setInterval(function () {
+      if(togAutoF){
+        fetch(url)
+        // returns json response
+        // somehow already knows how to pull 10 tweets
+        .then(function (response) {
+          return response.json();
+        })
+        // returns data of the json
+        .then(function (data) {
+          let html = "";
+          data.statuses.forEach(function (tweet) {
+            html += `
             <div class="tweet">
-          <img
-            src="${tweet.user.profile_image_url}"
-            alt="Profile Picture"
-            class="profile-picture"
-          />
-          <div>
-            <p><strong>${tweet.user.screen_name} </strong><span>${tweet.created_at}</span></p>
-            <caption>
-            ${tweet.text}
-            </caption>
-          </div>
-        </div>
-          `;
+              <img
+                src="${tweet.user.profile_image_url}"
+                alt="Profile Picture"
+                class="profile-picture"
+              />
+              <div>
+                <p><strong>${tweet.user.screen_name} </strong><span>${tweet.created_at}</span></p>
+                <caption>
+                ${tweet.text}
+                </caption>
+              </div>
+            </div>
+            `;
+          });
+          document.getElementById("tweets").innerHTML = html;
         });
-        document.getElementById("tweets").innerHTML = html;
-      });
-//   }, 5000);
+      }
+    }, 5000);
+}
+
+// pause/play loading JSON data
+
+function toggleFetch() {
+
+  togAutoF = !togAutoF;
+  console.log(togAutoF);
 }
